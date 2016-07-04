ed645821ab86f3de3888763d37667ce8b8589fc4
aleksander kutsenkov <ligalizer8@gmail.com>
2016-07-04 12:25:36 -0400

    f

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,1 +1,23 @@
 
+const router = require('express').Router();
+// const { createUser, loginUser } = require('../models/user');
+
+ const {saveFeedBack, } = require('../models/feedback');
+
+
+
+router.post('/save', saveFeedBack, function(req,res) {
+  console.log(req.body);
+  res.send({'reponse':'Done'});
+  // res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
